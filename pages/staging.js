e2f8f93275cc343a8a581c754d1f408625ae33ad
Cleber <cleberrich@192.168.1.3>
2023-02-22 12:58:48 -0300

    commit

---
+++ pages/staging.js
@@ -0,0 +1,17 @@
+const staging = () => {
+  return (
+    <div>
+      <iframe
+        src="https://player.vimeo.com/video/798978580?h=33bf3eaede&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        width="720"
+        height="1280"
+        frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowfullscreen
+        title="mobile.mp4"
+      ></iframe>
+    </div>
+  );
+};
+
+export default staging;
