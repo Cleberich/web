d2b3be7fa1114b45a6a66472a79238c7ed8a55ea
Cleber davila <cleberich@gmail.com>
2024-02-26 18:48:20 -0300

    commit

--- pages/cpanel/index.js
+++ pages/cpanel/index.js
@@ -2,7 +2,6 @@ import { useAuth } from "../../hooks/useAuth";
 
 export default function Home() {
   const auth = useAuth();
-  const { denegado } = useAuth();
 
   const handleGoogle = (e) => {
     e.preventDefault();
@@ -18,23 +17,19 @@ export default function Home() {
             Inicia sesión
           </h1>
 
-          {denegado == false && (
-            <>
-              <h2 className="text-[#191e21] text-md md:text-xl font-semibold mt-1 text-center">
-                Para ver tus contactos
-              </h2>
-              <img src="/img/google.jpeg" className="w-32 mx-auto" />
-              <button
-                className="w-full md:w-64 mt-12 py-4 mx-auto bg-red-800 hover:bg-red-600 text-white rounded-lg"
-                onClick={(e) => handleGoogle(e)}
-              >
-                Click para ingresar
-              </button>
-              <h3 className="text-xs text-black text-center mt-2">
-                Desarrollado por Cleber Davila
-              </h3>
-            </>
-          )}
+          <h2 className="text-[#191e21] text-md md:text-xl font-semibold mt-1 text-center">
+            Para ver tus contactos
+          </h2>
+          <img src="/img/google.jpeg" className="w-32 mx-auto" />
+          <button
+            className="w-full md:w-64 mt-12 py-4 mx-auto bg-red-800 hover:bg-red-600 text-white rounded-lg"
+            onClick={(e) => handleGoogle(e)}
+          >
+            Click para ingresar
+          </button>
+          <h3 className="text-xs text-black text-center mt-2">
+            Desarrollado por Cleber Davila
+          </h3>
         </div>
       </div>
     </div>
